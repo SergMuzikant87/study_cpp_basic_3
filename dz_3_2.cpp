f73d385fb4ee911ae7e5aa5c4bc31fc90c9f17b7
Musician <---------- ---------- ---------- ---------- ---------->
2025-11-16 19:34:11 +0300

    Исходный файл задачи 2. Форматирование кода.

--- dz_3_2.cpp
+++ dz_3_2.cpp
@@ -12,12 +12,12 @@ int main(void)
     std::cin >> value_3;
 
     std::cout << "Result is: \n";
-    (value_1 >= value_2) &&  (value_2 >= value_3)   ? std::cout << value_1 << " " << value_2 << " " << value_3 << "\n"
-    : (value_1 >= value_3) &&  (value_3 >= value_2) ? std::cout << value_1 << " " << value_3 << " " << value_2 << "\n" 
-    : (value_2 >= value_1) &&  (value_1 >= value_3) ? std::cout << value_2 << " " << value_1 << " " << value_3 << "\n"
-    : (value_2 >= value_3) &&  (value_3 >= value_1) ? std::cout << value_2 << " " << value_3 << " " << value_1 << "\n"  
-    : (value_3 >= value_2) &&  (value_2 >= value_1) ? std::cout << value_3 << " " << value_2 << " " << value_1 << "\n" 
-                                                  : std::cout << value_3 << " " << value_1 << " " << value_2 << "\n";
+      (value_1 >= value_2) && (value_2 >= value_3) ? std::cout << value_1 << " " << value_2 << " " << value_3 << "\n"
+    : (value_1 >= value_3) && (value_3 >= value_2) ? std::cout << value_1 << " " << value_3 << " " << value_2 << "\n" 
+    : (value_2 >= value_1) && (value_1 >= value_3) ? std::cout << value_2 << " " << value_1 << " " << value_3 << "\n"
+    : (value_2 >= value_3) && (value_3 >= value_1) ? std::cout << value_2 << " " << value_3 << " " << value_1 << "\n"  
+    : (value_3 >= value_2) && (value_2 >= value_1) ? std::cout << value_3 << " " << value_2 << " " << value_1 << "\n" 
+                                                   : std::cout << value_3 << " " << value_1 << " " << value_2 << "\n";
 
     return 0;
 }
